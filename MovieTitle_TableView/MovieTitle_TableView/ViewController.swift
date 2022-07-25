0fa77c1853f100002c6d66932670a8228bf4fe0b
sysloper <109938817+sysloper@users.noreply.github.com>
2022-07-25 20:19:50 +0900

    Add files via upload

--- MovieTitle_TableView/MovieTitle_TableView/ViewController.swift
+++ MovieTitle_TableView/MovieTitle_TableView/ViewController.swift
@@ -7,36 +7,35 @@
 
 import UIKit
 
-let movies = ["Inception","The Revenant","Titanic","Catch Me If You Can","Shutter Island","The Great Gatsby"]
-
-class CustomCell : UITableViewCell {
-    @IBOutlet var title1: UILabel!
-}
-
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    let movies = ["Inception","The Revenant","Titanic","Catch Me If You Can","Shutter Island","The Great Gatsby"]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomCell else {
-            return UITableViewCell()
-        }
-        cell.title1.text = movies[indexPath.row]
-    
-        return cell
+             return UITableViewCell()
+         }
+         cell.title1.text = movies[indexPath.row]
+         
+         return cell
+
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-
-    }
+      
     
+             }
 
 }
-
-
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var title1: UILabel!
+    
+}
 
